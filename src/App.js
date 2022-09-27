14888751fcd1b29686e35bfb29018cd087550278
mbashirli <mbashirlee@gmail.com>
2022-09-27 01:40:34 -0400

    finished header and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import HowToPlay from "./pages/HowToPlay";
+import FindFriends from "./pages/FindFriends";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/HowToPlay" element={<HowToPlay />} />
+        <Route path="/FindFriends" element={<FindFriends />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
